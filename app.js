cd8c2e7f5f7656ecaf24328a69c48af44244026b
unknown <arpitkapoor97@gmail.com>
2021-05-21 01:42:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const port = 5000
+
+
+//Static File
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/images', express.static(__dirname + 'public/images'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+//Templating Engine
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+//Body Parsing
+app.use(bodyParser.urlencoded({extended: true}))
+
+//Routes
+
+const newsRouter =  require('./src/routes/news')
+app.use('/',newsRouter)
+
+
+//Listening to port
+app.listen(port, () => console.log(`Listening on port...${port}`) )
---
+++ src/routes/news.js
@@ -0,0 +1,53 @@
+const express = require('express')
+
+const newsRouter = express.Router()
+
+const axios = require('axios')
+
+const api_key = '3dab794ab5ab40a792e41a1104e71a1f'
+
+newsRouter.get('', async(req,res)=>{
+    //res.render('news')
+    try {
+        const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${api_key}`)
+        //console.log(newsAPI.data)
+        res.render('news', {articles : newsAPI.data.articles})
+    } catch (err) {
+        if (err.response){
+            res.render('news', {articles : null})
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        }else if (err.request){
+            console.log(err.request)
+            res.render('news', {articles : null})
+        }
+        else{
+            console.log('Error',err.message)
+            res.render('news', {articles : null})
+        }
+    }   
+})
+
+newsRouter.post('', async(req,res)=> {
+    let search = req.body.search
+    try {
+        const newsAPI = await axios.get(`http://newsapi.org/v2/everything?q=${search}&sortBy=popularity&apiKey=${api_key}`)
+        res.render('newsSearch', {articles: newsAPI.data.articles})
+    } catch (err) {
+        if(err.response) {
+            res.render('newsSearch', { articles : null })
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if(err.requiest) {
+            res.render('newsSearch', { articles : null })
+            console.log(err.requiest)
+        } else {
+            res.render('newsSearch', { articles : null })
+            console.error('Error', err.message)
+        }
+    }
+})
+
+module.exports = newsRouter
